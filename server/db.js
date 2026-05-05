02f039d9654da146739708bfea9b49a288c0df18
David Kaplun <davidkaplun505@gmail.com>
2026-05-05 06:06:11 +0300

    now pg library accepts self-signed certificates

--- server/db.js
+++ server/db.js
@@ -2,5 +2,5 @@ import pg from 'pg'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false }})
 export default pool
